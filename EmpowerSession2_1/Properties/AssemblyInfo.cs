8956e87c80d2b58d3400cff778bc7187a74cdff7
ThomasWZ <thomas.warnez@skyline.be>
2023-03-28 10:38:09 +0200

    Add project files.

---
+++ EmpowerSession2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSession2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSession2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("336FFBD7-C9DB-401E-A7A5-168CD4366846")]
+[assembly: AssemblyVersion("1.0.0.0")]
